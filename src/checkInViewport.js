8f0fc51be3d0f4f535fc88fe7edfdcf3b1278010
JungSeoro <jsr@jungseoro.com>
2023-01-15 20:48:23 +0900

    Remove checkInViewport

--- src/checkInViewport.js
+++
@@ -1,7 +0,0 @@
-export const checkInViewport = (elem) => {
-    if (!elem || !window) {
-        return false;
-    }
-    const { top: elementTop, bottom: elementBottom, } = elem.getBoundingClientRect();
-    return elementBottom > 0 && elementTop <= window.innerHeight;
-};
